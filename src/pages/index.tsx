dbecef1e5a5ebf2200332c3ad92912ac7df812d8
Josebautista10 <cooldj1020@gmail.com>
2023-02-28 21:02:45 -0700

    use deployed stapi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import { Inter } from '@next/font/google'
-
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [barbers, setBarbers] = useState()
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/barbers`)
+      .then((response) => response.json())
+      .then((data) => setBarbers(data))
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  },[])
+  console.log(barbers)
+  
+  // const mappedBarbers = barbers.length > 0 && barbers.map(barber => {
+  //   return (
+  //     <ul>
+  //       <li>{barber}</li>
+  //     </ul>
+  //   )
+  // })
   return (
     <>
-    <h1>Hello world!</h1>
-      </>
+      {/* <h1>{barbers? mappedBarbers: 'loading'}</h1> */}
+    </>
   )
 }
